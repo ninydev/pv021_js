17e48889f5fc22568613857d3335fee6d54036c1
Olexander Nykytin <keeper@ninydev.com>
2021-12-01 19:22:49 +0200

    Form +

--- forForm.php
+++ forForm.php
@@ -1,4 +1,6 @@
 <?php
+//$_SERVER['REQUEST_TIME']
+setcookie("LastRequest", date('Y-m-d H:m:s'), time()+3600);  /* срок действия 1 час */
 header('Content-Type:  text/plain; charset=utf-8');
 
 echo "\nCOOKIE:\n";
