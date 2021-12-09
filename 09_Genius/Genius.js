23de588611f0c2b488027b41764d21250f6744fb
nikitin_a <nikitin_a@itstep.academy>
2021-12-09 19:14:28 +0200

    Genius getFromServer

--- 09_Genius/Genius.js
+++ 09_Genius/Genius.js
@@ -5,12 +5,36 @@ class Genius{
 
     constructor() {
         this.baseId = "app_";
-        console.log(this.baseId + "Result");
+    }
+
+    items = [];
+
+    getFromServer(){
+        fetch("https://genius.p.rapidapi.com/search?q=" +
+            encodeURI(document.getElementById(this.baseId + "txtSearch").value),
+            {
+            "method": "GET",
+            "headers": {
+                "x-rapidapi-host": "genius.p.rapidapi.com",
+                "x-rapidapi-key": "7e2afce0b1msh7f5c23d92a82e63p15d2e7jsn1fb4d1a13ff8"
+            }
+        })
+            .then(response => {
+                console.log(response);
+                return response.json();
+            })
+            .then(data => {
+                console.log(data);
+                this.items = data;
+            })
+            .catch(err => {
+                console.error(err);
+            });
     }
 
     doSearch(){
-        console.log(this.baseId + "Result");
-        document.getElementById(this.baseId + "Result").innerHTML = " Ready to Search";
+        this.getFromServer();
+        //document.getElementById(this.baseId + "Result").innerHTML = " Ready to Search";
     }
 
     render (homeElementId){
